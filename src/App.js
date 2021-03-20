bd4b874c3d9755901e95a07d84f3a220372188de
Atchara Yawirach <atchara2571@gmail.com>
2021-03-20 16:23:55 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>I'll continue working on this</h1>
+      <p>when I feel like I want to</p>
+  </div>
+  );
+}
+
+export default App;
